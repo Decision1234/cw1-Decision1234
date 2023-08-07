1fef68f07b70aa57eb83c170516a6db96812dc44
Decision1234 <ghimiredecision@gmail.com>
2023-08-07 20:02:54 +0545

    some changes

--- contact.php
+++ contact.php
@@ -6,42 +6,43 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/7f257b9379.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="./css/style.css">
-    <title>Contact</title>
+    <title>About</title>
 </head>
-<body>
-    <nav class="navbar bg-dark">
-        <div class="container">
-            <h1 class="logo">N.T</h1>
-            <ul class="nav-items">
+<body>  
+        <nav class="navbar bg-dark">
+            <div class="container">
+
+                <h1 class="logo">Yatri</h1>
+                <ul class="nav-items">
                 <li class="nav-item"><a href="./index.html">Home</a></li>
                 <li class="nav-item"><a href="./about.html">About</a></li>
+                <li class="nav-item"><a href="./Project-0.html">Project-0</a></li>
+                <li class="nav-item"><a href="./Project-1.html">Project-1</a></li>
                 <li class="nav-item"><a href="./contact.html">Contact</a></li>
-                <li class="nav-item"><a href="./my-login.html">Sign Up / Login</a></li>
+                <li class="nav-item"><a href="./login.html"> Login</a></li>
             </ul>
-        </div>
+
+            </div>
     </nav>
 
     <section class="contact-form">
         <div class="container-contact">
             <div class="form-wrapper">
                 <div class="company-address">
-                    <i class="fas fa-map-marker-alt fa-3x" style="color: #ee4758;"></i>
-                    <h2>LOCATION</h2>
-                    <p>10th floor, M.G Road, Bangalore-461365</p>
-                    <i class="fas fa-envelope fa-3x" style="color: #ee4758;"></i>
-                    <h2>E-MAIL</h2>
-                    <p>travelnext@domain.com</p>
-                    <i class="fas fa-phone-square-alt fa-3x" style="color: #ee4758;"></i>
-                    <h2>CALL</h2>
-                    <p>+91-1150568508</p>
+                    <i class="fas fa-map-marker-alt fa-3x" style="color: #05b8ff;"></i>
+                    <p>ADDRESS:44616 Thirbam Sadak, Baluwatar Kathmandu, Nepal</p>
+                    <i class="fas fa-map-marker-alt fa-3x" style="color: #05b8ff;"></i>
+                    <p>ADDRESS:44600, Budanilkantha Kathmandu, Nepal</p>
+                    <i class="fas fa-envelope fa-3x" style="color: #05b8ff;"></i>
+                    <p>info@yatrimotorcycles.com</p>
+                    <i class="fas fa-phone-square-alt fa-3x" style="color: #05b8ff;"></i>
+                    <p> +977 (980) 187-7447</p>
 
-                    <img src="./img/office.jpg" alt="Company Image">
+                    <img src=".\img\images.jpg" alt="Company Image">
                 </div>
                 <form action="" class="form">
                     <h1>Contact Us</h1>
-                    <p>Let us know your questions, suggestions and concerns by filling out the contact form below</p>
-
-                    <div class="form-group">
+                      <div class="form-group">
                         <label for="username">Name</label>
                         <input type="text" name="" id="username" required>
                     </div>
@@ -59,6 +60,12 @@
                     </div>
                     <button type="submit" class="form-btn">Submit</button>
                 </form>
+                <div class="col-sm-8">
+                    <div class="map-responsive">
+                       </div>
+                    </p>
+                       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3530.194092376108!2d85.36266917519654!3d27.77299312259364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb1d8c6aafd8d9%3A0xfefe2648d451db4!2sYatri%20Motorcycles!5e0!3m2!1sen!2snp!4v1688628194199!5m2!1sen!2snp" width="1000" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </p>
             </div>
         </div>
     </section>
@@ -69,7 +76,7 @@
             <i class="fab fa-twitter fa-4x"></i>
             <i class="fab fa-instagram fa-4x"></i>
         </div>
-        <p>Nepal Travels &copy; 2023, All Rights Reserved</p>
+        <p>Yatri &copy; 2023, All Rights Reserved</p>
     </footer>
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,25 +1,59 @@
-<!doctype html>
-<html lang="ar" dir="rtl">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tailwind Login Template</title>
+    <meta name="author" content="David Grzyb">
+    <meta name="description" content="">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.rtl.min.css" integrity="sha384-PJsj/BTMqILvmcej7ulplguok8ag4xFTPryRq8xevL7eBYSmpXKcbNVuy+P0RMgq" crossorigin="anonymous">
+    <!-- Tailwind -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet">
+    <style>
+        @import url('https://fonts.googleapis.com/css?family=Karla:400,700&display=swap');
 
-    <title>login</title><body>
-   <?php require 'phpfolder/_nav.php'?>
+        .font-family-karla {
+            font-family: karla;
+        }
+    </style>
+</head>
+<body class="bg-white font-family-karla h-screen">
 
-    <!-- Optional JavaScript; choose one of the two! -->
+    <div class="w-full flex flex-wrap">
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+        <div class="w-full md:w-1/2 flex flex-col">
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
-    -->
-  </body>
+            <div class="flex justify-center md:justify-start pt-12 md:pl-12 md:-mb-24">
+                <a href="#" class="bg-black text-white font-bold text-xl p-4">Logo</a>
+            </div>
+
+            <div class="flex flex-col justify-center md:justify-start my-auto pt-8 md:pt-0 px-8 md:px-24 lg:px-32">
+                <p class="text-center text-3xl">Welcome.</p>
+                <form class="flex flex-col pt-3 md:pt-8" onsubmit="event.preventDefault();">
+                    <div class="flex flex-col pt-4">
+                        <label for="email" class="text-lg">Email</label>
+                        <input type="email" id="email" placeholder="your@email.com" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline">
+                    </div>
+    
+                    <div class="flex flex-col pt-4">
+                        <label for="password" class="text-lg">Password</label>
+                        <input type="password" id="password" placeholder="Password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline">
+                    </div>
+    
+                    <input type="submit" value="Log In" class="bg-black text-white font-bold text-lg hover:bg-gray-700 p-2 mt-8">
+                </form>
+                <div class="text-center pt-12 pb-12">
+                    <p>Don't have an account? <a href="signin.html" class="underline font-semibold">Register here.</a></p>
+                </div>
+            </div>
+
+        </div>
+
+     
+        <div class="w-1/2 shadow-2xl">
+            <img class="object-cover w-full h-screen hidden md:block" src="https://source.unsplash.com/IXUM4cJynP0">
+        </div>
+    </div>
+
+</body>
 </html>
