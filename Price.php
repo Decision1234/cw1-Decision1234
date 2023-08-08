42d92c8436c0833c49f82b04cc8475100a9896b3
Decision1234 <117433671+Decision1234@users.noreply.github.com>
2023-08-08 13:19:32 +0545

    Frontend

---
+++ Price.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/7f257b9379.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Project-</title>
+</head>
+<body>  
+    <nav class="navbar bg-dark">
+        <div class="container">
+
+            <h1 class="logo">Yatri</h1>
+            <ul class="nav-items">
+            <li class="nav-item"><a href="./index.html">Home</a></li>
+            <li class="nav-item"><a href="./about.html">About</a></li>
+            <li class="nav-item"><a href="./Project-0.html">Project-0</a></li>
+            <li class="nav-item"><a href="./Project-1.html">Project-1</a></li>
+            <li class="nav-item"><a href="./contact.html">Contact</a></li>
+            <li class="nav-item"><a href="./login.html"> Login</a></li>
+        </ul>
+
+        </div>
+    </nav>
+      <div>
+             <section class="showcase" id="explore">
+                <div class="container">
+                    <div class="row11 row2">
+                    <div class="img-box">
+                        <img src="./img/Yatri-P1-Side-Styling.png" alt="Project-1">
+                    </div>
+                    <h2>&nbsp;&nbsp;Project-1</h2>
+                        <h1 class="center_text">&nbsp;&nbsp;PRICE <span style="color: #09cef5;">Rs. 5,65,000 </span></h1>
+                        <h1 class="center_text">&nbsp;&nbsp;BATTERY...........<span style="color: #09cef5;">3.0kWh</span></h1>
+                        <h1 class="center_text">&nbsp;&nbsp;PEAK POWER........ <span style="color: #09cef5;">19 HP</span></h1>
+                        <h1 class="center_text">&nbsp;&nbsp;TORQUE............<span style="color: #09cef5;">480 Nm (at the wheel)</span></h1>
+                       <h1 class="center_text">&nbsp;&nbsp;RANGE............. <span style="color: #09cef5;">110 Km</span></h1>
+                       <h1 class="center_text">&nbsp;&nbsp;CHARGING TIME..... <span style="color: #09cef5;">2 hours (40 mins with Fast Charging)</span></h1>
+                       <h1 class="center_text">&nbsp;&nbsp;TOP SPEED <span style="color: #09cef5;">100 Kmph</span></h1>
+                       <h1 class="center_text">&nbsp;&nbsp;BRAKING TYPE...... <span style="color: #09cef5;">Regenerative Braking System</span></h1>
+                       <h1 class="center_text">&nbsp;&nbsp;SUPSPENSION... <span style="color: #09cef5;">USD Forks and Monoshock</span></h1>
+                       <h1 class="center_text">&nbsp;&nbsp;WEIGHT.............<span style="color: #09cef5;">120 Kg</span></h1>
+                       <h1 class="center_text">&nbsp;&nbsp;SEAT HEIGHT........ <span style="color: #09cef5;">840 mm</span></h1>
+                       <h1 class="center_text">&nbsp;&nbsp;GROUND CLEARANCE... <span style="color: #09cef5;">320 mm</span></h1> 
+                       <p style="text-align: center;"><a href="./contact.html" class="button-secondary">Order now</a></p>   
+               </div>
+              </div>
+                    <div class="container">
+                        <div class="row11 row2">
+                        <div class="img-box">
+                            <img src=".\img\Project-Zero.png" alt="Project-0">
+                        </div>
+                    
+                            <h2>&nbsp;&nbsp;Project-0</h2>
+                            <h1 class="center_text">&nbsp;&nbsp;PRICE <span style="color: #09cef5;">Rs. 19,49,000</span></h1>
+                            <h1 class="center_text">&nbsp;&nbsp;BATTERY...........<span style="color: #09cef5;">8.0 kW Lithium-Ion</span></h1>
+                           <h1 class="center_text">&nbsp;&nbsp;PEAK POWER........ <span style="color: #09cef5;">48 kW (64 hp)</span></h1>
+                           <h1 class="center_text">&nbsp;&nbsp;TORQUE............<span style="color: #09cef5;">120 Nm at shaft (650 Nm at wheel)</span></h1>
+                           <h1 class="center_text">&nbsp;&nbsp;ACCELERATION...... <span style="color: #09cef5;">0-60 km/h in under 2.5s</span></h1>
+                           <h1 class="center_text">&nbsp;&nbsp;RANGE............. <span style="color: #09cef5;">230 Km</span></h1>
+                           <h1 class="center_text">&nbsp;&nbsp;CHARGING TIME..... <span style="color: #09cef5;">2 hours | 3.3 kW charger onboard</span></h1>
+                           <h1 class="center_text">&nbsp;&nbsp;TOP SPEED........<span style="color: #09cef5;">140 Kmph</span></h1>
+                           <h1 class="center_text">&nbsp;&nbsp;TYRES.............<span style="color: #09cef5;">Metzeler Sportec M7RR with Aluminium Rims</span></h1>
+                           <h1 class="center_text">&nbsp;&nbsp;BRAKING TYPE...... <span style="color: #09cef5;">Brembo E-Braking</span></h1>
+                           <h1 class="center_text">&nbsp;&nbsp;FRONT SUPSPENSION.. <span style="color: #09cef5;">43 mm RCP adjustable USD Forks</span></h1>
+                           <h1 class="center_text">&nbsp;&nbsp;REAR SUPSPENSION... <span style="color: #09cef5;">RC adjustable Nitrogen-Charged Monoshock</span></h1>
+                           <h1 class="center_text">&nbsp;&nbsp;WEIGHT.............<span style="color: #09cef5;">190 Kg</span></h1>
+                           <h1 class="center_text">&nbsp;&nbsp;SEAT HEIGHT........ <span style="color: #09cef5;">800 mm</span></h1>
+                           <h1 class="center_text">&nbsp;&nbsp;GROUND CLEARANCE... <span style="color: #09cef5;">175 mm</span></h1>
+                           <h1 class="center_text">&nbsp;&nbsp;INTERNET...........<span style="color: #09cef5;">On-board 4G network</span></h1>
+                           <h1 class="center_text">&nbsp;&nbsp;MOBILE APP......... <span style="color: #09cef5;">Android and IOS | Antitheft with on-board GPS</span></h1> 
+                           <p style="text-align: center;"><a href="./contact.html" class="button-secondary">Order now</a></p>  
+                   </div>
+                  </div>
+                </div>
+                </div>
+               </div>
+               </div>
+               </div>
+               </div>
+    </section>
+    <footer class=" footer">
+        <div class="social-media-links">
+            <i class="fab fa-facebook fa-4x"></i>
+            <i class="fab fa-twitter fa-4x"></i>
+            <i class="fab fa-instagram fa-4x"></i>
+        </div>
+        <p>Yatri &copy; 2023, All Rights Reserved</p>
+    </footer>
+</body>
+</html>
+
+
+
