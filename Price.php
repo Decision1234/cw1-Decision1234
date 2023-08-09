45097c139ece95a6f5d1feaf8d61ae992926c654
Decision1234 <117433671+Decision1234@users.noreply.github.com>
2023-08-09 10:31:37 +0545

    login page update

--- Price.php
+++ Price.php
@@ -22,63 +22,64 @@
             <li class="nav-item"><a href="login.php"> Login</a></li>
         </ul>
 
-        </div>
-    </nav>
-      <div>
-      <div class="container">
-                        <div class="row11 row2">
+                 </div>
+               </nav>
+                  <div>
+
+                   </div>
+                     <div class="container">
                         <div class="img-box">
-                            <img src=".\img\P1.png" alt="Project-0">
+                            <img src="img\Project-Zero.png" alt="Project-0"style="width:50%;height:100%;margin-top: 2rem;">
                         </div>
-                    <h2>&nbsp;&nbsp;Project-1</h2>
-                        <h1 class="center_text">&nbsp;&nbsp;PRICE <span style="color: #09cef5;">Rs. 5,65,000 </span></h1>
-                        <h1 class="center_text">&nbsp;&nbsp;BATTERY...........<span style="color: #09cef5;">3.0kWh</span></h1>
-                        <h1 class="center_text">&nbsp;&nbsp;PEAK POWER........ <span style="color: #09cef5;">19 HP</span></h1>
-                        <h1 class="center_text">&nbsp;&nbsp;TORQUE............<span style="color: #09cef5;">480 Nm (at the wheel)</span></h1>
-                       <h1 class="center_text">&nbsp;&nbsp;RANGE............. <span style="color: #09cef5;">110 Km</span></h1>
-                       <h1 class="center_text">&nbsp;CHARGING TIME..... <span style="color: #09cef5;">2 hours (40 mins with Fast Charging)</span></h1>
-                       <h1 class="center_text">&nbsp;&nbsp;TOP SPEED <span style="color: #09cef5;">100 Kmph</span></h1>
-                       <h1 class="center_text">&nbsp;&nbsp;BRAKING TYPE...... <span style="color: #09cef5;">Regenerative Braking System</span></h1>
-                       <h1 class="center_text">&nbsp;&nbsp;SUPSPENSION... <span style="color: #09cef5;">USD Forks and Monoshock</span></h1>
-                       <h1 class="center_text">&nbsp;&nbsp;WEIGHT.............<span style="color: #09cef5;">120 Kg</span></h1>
-                       <h1 class="center_text">&nbsp;&nbsp;SEAT HEIGHT........ <span style="color: #09cef5;">840 mm</span></h1>
-                       <h1 class="center_text">&nbsp;&nbsp;GROUND CLEARANCE... <span style="color: #09cef5;">320 mm</span></h1> 
-                       <p style="text-align: center;"><a href="contact.php" class="button-secondary">Order now</a></p>   
-               </div>
-              </div>
-                    <div class="container">
-                        <div class="row11 row2">
-                        <div class="img-box">
-                            <img src=".\img\Project-Zero.png" alt="Project-0">
-                        </div>
-                    
-                            <h2>&nbsp;&nbsp;Project-0</h2>
-                            <h1 class="center_text">&nbsp;&nbsp;PRICE <span style="color: #09cef5;">Rs. 19,49,000</span></h1>
-                            <h1 class="center_text">&nbsp;&nbsp;BATTERY...........<span style="color: #09cef5;">8.0 kW Lithium-Ion</span></h1>
-                           <h1 class="center_text">&nbsp;&nbsp;PEAK POWER........ <span style="color: #09cef5;">48 kW (64 hp)</span></h1>
-                           <h1 class="center_text">&nbsp;&nbsp;TORQUE............<span style="color: #09cef5;">120 Nm at shaft (650 Nm at wheel)</span></h1>
-                           <h1 class="center_text">&nbsp;&nbsp;ACCELERATION...... <span style="color: #09cef5;">0-60 km/h in under 2.5s</span></h1>
-                           <h1 class="center_text">&nbsp;&nbsp;RANGE............. <span style="color: #09cef5;">230 Km</span></h1>
-                           <h1 class="center_text">&nbsp;&nbsp;CHARGING TIME..... <span style="color: #09cef5;">2 hours | 3.3 kW charger onboard</span></h1>
-                           <h1 class="center_text">&nbsp;&nbsp;TOP SPEED........<span style="color: #09cef5;">140 Kmph</span></h1>
-                           <h1 class="center_text">&nbsp;&nbsp;TYRES.............<span style="color: #09cef5;">Metzeler Sportec M7RR with Aluminium Rims</span></h1>
-                           <h1 class="center_text">&nbsp;&nbsp;BRAKING TYPE...... <span style="color: #09cef5;">Brembo E-Braking</span></h1>
-                           <h1 class="center_text">&nbsp;&nbsp;FRONT SUPSPENSION.. <span style="color: #09cef5;">43 mm RCP adjustable USD Forks</span></h1>
-                           <h1 class="center_text">&nbsp;&nbsp;REAR SUPSPENSION... <span style="color: #09cef5;">RC adjustable Nitrogen-Charged Monoshock</span></h1>
-                           <h1 class="center_text">&nbsp;&nbsp;WEIGHT.............<span style="color: #09cef5;">190 Kg</span></h1>
-                           <h1 class="center_text">&nbsp;&nbsp;SEAT HEIGHT........ <span style="color: #09cef5;">800 mm</span></h1>
-                           <h1 class="center_text">&nbsp;&nbsp;GROUND CLEARANCE... <span style="color: #09cef5;">175 mm</span></h1>
-                           <h1 class="center_text">&nbsp;&nbsp;INTERNET...........<span style="color: #09cef5;">On-board 4G network</span></h1>
-                           <h1 class="center_text">&nbsp;&nbsp;MOBILE APP......... <span style="color: #09cef5;">Android and IOS | Antitheft with on-board GPS</span></h1> 
+
+                            <h2>Project-0</h2>
+                            <h1 class="center_text">PRICE <span style="color: #09cef5;">Rs. 19,49,000</span></h1>
+                            <h1 class="center_text">BATTERY...........<span style="color: #09cef5;">8.0 kW Lithium-Ion</span></h1>
+                           <h1 class="center_text">PEAK POWER........ <span style="color: #09cef5;">48 kW (64 hp)</span></h1>
+                           <h1 class="center_text">TORQUE............<span style="color: #09cef5;">120 Nm at shaft (650 Nm at wheel)</span></h1>
+                           <h1 class="center_text">ACCELERATION...... <span style="color: #09cef5;">0-60 km/h in under 2.5s</span></h1>
+                           <h1 class="center_text">RANGE............. <span style="color: #09cef5;">230 Km</span></h1>
+                           <h1 class="center_text">CHARGING TIME..... <span style="color: #09cef5;">2 hours | 3.3 kW charger onboard</span></h1>
+                           <h1 class="center_text">TOP SPEED........<span style="color: #09cef5;">140 Kmph</span></h1>
+                           <h1 class="center_text">TYRES.............<span style="color: #09cef5;">Metzeler Sportec M7RR with Aluminium Rims</span></h1>
+                           <h1 class="center_text">BRAKING TYPE...... <span style="color: #09cef5;">Brembo E-Braking</span></h1>
+                           <h1 class="center_text">FRONT SUPSPENSION.. <span style="color: #09cef5;">43 mm RCP adjustable USD Forks</span></h1>
+                           <h1 class="center_text">REAR SUPSPENSION... <span style="color: #09cef5;">RC adjustable Nitrogen-Charged Monoshock</span></h1>
+                           <h1 class="center_text">WEIGHT.............<span style="color: #09cef5;">190 Kg</span></h1>
+                           <h1 class="center_text">SEAT HEIGHT........ <span style="color: #09cef5;">800 mm</span></h1>
+                           <h1 class="center_text">GROUND CLEARANCE... <span style="color: #09cef5;">175 mm</span></h1>
+                           <h1 class="center_text">INTERNET...........<span style="color: #09cef5;">On-board 4G network</span></h1>
+                           <h1 class="center_text">MOBILE APP......... <span style="color: #09cef5;">Android and IOS | Antitheft with on-board GPS</span></h1> 
                            <p style="text-align: center;"><a href="contact.php" class="button-secondary">Order now</a></p>  
+
                    </div>
                   </div>
-                </div>
-                </div>
+                </div> 
+                <div class="container"> 
+                        <div class="img-box">
+                            <img src=".\img\p1.png" alt="Project-1"style="width:60%;height:100%;margin-top: 4rem;">
+                        </div>
+               <h2>Project-1</h2>
+                        <h1 class="center_text">PRICE <span style="color: #09cef5;">Rs. 5,65,000 </span></h1>
+                        <h1 class="center_text">BATTERY...........<span style="color: #09cef5;">3.0kWh</span></h1>
+                        <h1 class="center_text">PEAK POWER........ <span style="color: #09cef5;">19 HP</span></h1>
+                        <h1 class="center_text">TORQUE............<span style="color: #09cef5;">480 Nm (at the wheel)</span></h1>
+                       <h1 class="center_text">RANGE............. <span style="color: #09cef5;">110 Km</span></h1>
+                       <h1 class="center_text">CHARGING TIME..... <span style="color: #09cef5;">2 hours (40 mins with Fast Charging)</span></h1>
+                       <h1 class="center_text">TOP SPEED <span style="color: #09cef5;">100 Kmph</span></h1>
+                       <h1 class="center_text">BRAKING TYPE...... <span style="color: #09cef5;">Regenerative Braking System</span></h1>
+                       <h1 class="center_text">SUPSPENSION... <span style="color: #09cef5;">USD Forks and Monoshock</span></h1>
+                       <h1 class="center_text">WEIGHT.............<span style="color: #09cef5;">120 Kg</span></h1>
+                       <h1 class="center_text">SEAT HEIGHT........ <span style="color: #09cef5;">840 mm</span></h1>
+                       <h1 class="center_text">GROUND CLEARANCE... <span style="color: #09cef5;">320 mm</span></h1> 
+                       <p style="text-align: center;"><a href="contact.php" class="button-secondary">Order now</a></p>   
+               </div>
+               </div> 
                </div>
                </div>
                </div>
                </div>
+               
     </section>
     <footer class=" footer">
         <div class="social-media-links">
